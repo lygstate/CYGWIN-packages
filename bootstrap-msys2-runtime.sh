508c976566857eef5a5e4b55153cdfc0648bafe1
Yonggang Luo <luoyonggang@gmail.com>
2026-01-04 11:47:32 +0800

    Install gcc properly

--- bootstrap-msys2-runtime.sh
+++ bootstrap-msys2-runtime.sh
@@ -1,6 +1,6 @@
 
 pushd ./ports/msys2-runtime
-updpkgsums
+# updpkgsums
 makepkg --cleanbuild --syncdeps --force --noconfirm
 mv -f msys2-runtime-devel-3.6.5.1-1-x86_64.pkg.tar.zst ../../dist/msys2-runtime-devel-3.6.5.1-1-x86_64-stage0.pkg.tar.zst
 mv -f msys2-runtime-3.6.5.1-1-x86_64.pkg.tar.zst ../../dist/msys2-runtime-3.6.5.1-1-x86_64-stage0.pkg.tar.zst
@@ -9,7 +9,7 @@ tar xf ../../dist/gcc-libs-15.2.0-2-x86_64-stage0.pkg.tar.zst  -C /
 tar xf ../../dist/msys2-runtime-devel-3.6.5.1-1-x86_64-stage0.pkg.tar.zst  -C /
 tar xf ../../dist/msys2-runtime-3.6.5.1-1-x86_64-stage0.pkg.tar.zst  -C /
 pacman -U  ../../dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst --noconfirm
-pacman -U  ../../dist/gcc-15.2.0-2-x86_64-stage0.pkg.tar.zst --noconfirm
+tar xf ../../dist/gcc-15.2.0-2-x86_64-stage0.pkg.tar.zst -C /
 
 makepkg --cleanbuild --syncdeps --force --noconfirm
 mv -f msys2-runtime-devel-3.6.5.1-1-x86_64.pkg.tar.zst ../../dist/
