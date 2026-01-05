7e62453ec8eee19f3c0abedcd5c1fc50d45e639a
Yonggang Luo <luoyonggang@gmail.com>
2026-01-06 05:11:20 +0800

    update bootstrap-extras.sh

--- bootstrap-extras.sh
+++ bootstrap-extras.sh
@@ -2,3 +2,23 @@ pushd ./ports/gmp
 makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
 find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
 popd
+
+pushd ./ports/isl
+makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
+find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
+popd
+
+pushd ./ports/mpc
+makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
+find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
+popd
+
+pushd ./ports/mpfr
+makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
+find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
+popd
+
+pushd ./ports/zlib
+makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
+find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
+popd
