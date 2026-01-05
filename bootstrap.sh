7e8ea411793f0868f354a0d4c96fcb22760ba650
Yonggang Luo <luoyonggang@gmail.com>
2026-01-05 13:22:25 +0800

    bootstraping

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,3 +25,10 @@ mv -f gcc-15.2.0-2-x86_64.pkg.tar.zst ../../dist/gcc-15.2.0-2-x86_64-stage0.pkg.
 popd
 
 sh bootstrap-msys2-runtime.sh
+
+pushd ./ports/gcc
+# updpkgsums
+makepkg --cleanbuild --syncdeps --force --noconfirm
+mv -f gcc-libs-15.2.0-2-x86_64.pkg.tar.zst ../../dist/gcc-libs-15.2.0-2-x86_64-stage1.pkg.tar.zst
+mv -f gcc-15.2.0-2-x86_64.pkg.tar.zst ../../dist/gcc-15.2.0-2-x86_64-stage1.pkg.tar.zst
+popd
--- install-bootstrap.sh
+++ install-bootstrap.sh
@@ -3,8 +3,8 @@ pacman -U  ./dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst --noconfirm
 tar xf ./dist/gcc-libs-15.2.0-2-x86_64-stage0.pkg.tar.zst  -C /
 tar xf ./dist/gcc-15.2.0-2-x86_64-stage0.pkg.tar.zst -C /
 
-tar xf ./dist/msys2-runtime-devel-3.6.5.1-1-x86_64.pkg.tar.zst  -C /
-tar xf ./dist/msys2-runtime-3.6.5.1-1-x86_64.pkg.tar.zst  -C /
+tar xf ./dist/msys2-runtime-devel-3.6.5.2-1-x86_64.pkg.tar.zst  -C /
+tar xf ./dist/msys2-runtime-3.6.5.2-1-x86_64.pkg.tar.zst  -C /
 
 tar xf ./dist/libtool-2.5.4-5-x86_64.pkg.tar.zst -C /
 
