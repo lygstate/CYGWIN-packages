20868116bd6655e48dcf027b1e0a2f0fcb9de6bb
Yonggang Luo <luoyonggang@gmail.com>
2026-01-06 03:31:42 +0800

    bootstrap gcc properly

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,24 +9,24 @@ rm -rf /usr/lib/*.origin
 # Building and install cmake
 pushd ./ports/cmake
 # updpkgsums
-makepkg --cleanbuild --syncdeps --force --noconfirm --install 
+makepkg --cleanbuild --syncdeps --force --noconfirm
 find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
 popd
 
 # Building and install libtool
 pushd ./ports/libtool
-makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck --install 
+makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
 find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
 popd
+pacman -U --overwrite \* ./dist/libltdl-2.5.4-5-x86_64.pkg.tar.zst --noconfirm
+pacman -U --overwrite \* ./dist/libtool-2.5.4-5-x86_64.pkg.tar.zst --noconfirm
+pacman -U --overwrite \* ./dist/cmake-4.2.1-2-x86_64.pkg.tar.zst --noconfirm
 
 # Building/install binutils
 pushd ./ports/binutils
 makepkg --cleanbuild --syncdeps --force --noconfirm
 mv -f binutils-2.45.1-2-x86_64.pkg.tar.zst ../../dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst
 popd
-pacman -U --overwrite \* ./dist/cmake-4.2.1-2-x86_64.pkg.tar.zst --noconfirm
-pacman -U --overwrite \* ./dist/libltdl-2.5.4-5-x86_64.pkg.tar.zst --noconfirm
-pacman -U --overwrite \* ./dist/libtool-2.5.4-5-x86_64.pkg.tar.zst --noconfirm
 pacman -U --overwrite \* ./dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst --noconfirm
 
 sh bootstrap-msys2-runtime.sh
--- install-bootstrap.sh
+++ install-bootstrap.sh
@@ -1,12 +1,10 @@
-pacman -U  ./dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst --noconfirm
-
-tar xf ./dist/gcc-libs-15.2.0-2-x86_64-stage0.pkg.tar.zst  -C /
-tar xf ./dist/gcc-15.2.0-2-x86_64-stage0.pkg.tar.zst -C /
+pacman -U --overwrite \* ./dist/cmake-4.2.1-2-x86_64.pkg.tar.zst --noconfirm
+pacman -U --overwrite \* ./dist/libltdl-2.5.4-5-x86_64.pkg.tar.zst --noconfirm
+pacman -U --overwrite \* ./dist/libtool-2.5.4-5-x86_64.pkg.tar.zst --noconfirm
+pacman -U --overwrite \* ./dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst --noconfirm
 
 tar xf ./dist/msys2-runtime-devel-3.6.5.2-1-x86_64.pkg.tar.zst  -C /
 tar xf ./dist/msys2-runtime-3.6.5.2-1-x86_64.pkg.tar.zst  -C /
-
-tar xf ./dist/libtool-2.5.4-5-x86_64.pkg.tar.zst -C /
 
 tar xf ./dist/libiconv-devel-1.18-2-x86_64.pkg.tar.zst -C /
 tar xf ./dist/libiconv-1.18-2-x86_64.pkg.tar.zst -C /
