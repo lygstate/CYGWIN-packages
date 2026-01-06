c44b5cfbd335282190a86095f95fc7a0df620977
Yonggang Luo <luoyonggang@gmail.com>
2026-01-06 15:15:14 +0800

    msys2-runtime shrink.

--- bootstrap-extras.sh
+++ bootstrap-extras.sh
@@ -1,3 +1,11 @@
+pushd ./ports/libiconv
+makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
+find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
+popd
+
+tar xf ./dist/libiconv-devel-1.18-2-x86_64.pkg.tar.zst -C /
+tar xf ./dist/libiconv-1.18-2-x86_64.pkg.tar.zst -C /
+
 pushd ./ports/gmp
 makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
 find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
--- bootstrap-msys2-runtime.sh
+++ bootstrap-msys2-runtime.sh
@@ -6,11 +6,3 @@ popd
 
 tar xf ./dist/msys2-runtime-devel-3.6.5.2-1-x86_64.pkg.tar.zst  -C /
 tar xf ./dist/msys2-runtime-3.6.5.2-1-x86_64.pkg.tar.zst  -C /
-
-pushd ./ports/libiconv
-makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
-find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
-popd
-
-tar xf ./dist/libiconv-devel-1.18-2-x86_64.pkg.tar.zst -C /
-tar xf ./dist/libiconv-1.18-2-x86_64.pkg.tar.zst -C /
