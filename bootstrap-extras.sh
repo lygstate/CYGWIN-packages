5079fe8ad085ecb1cb6d9e6b3f63d130bbbc97e2
Yonggang Luo <luoyonggang@gmail.com>
2026-01-06 08:34:00 +0800

    gmp building fine

--- bootstrap-extras.sh
+++ bootstrap-extras.sh
@@ -1,3 +1,7 @@
+pushd ./ports/libtool
+makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck --install
+popd
+
 pushd ./ports/gmp
 makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
 find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
