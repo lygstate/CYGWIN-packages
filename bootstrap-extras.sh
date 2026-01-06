3758922fbad2ba181b7e78f8a74dd37b99485bdf
Yonggang Luo <luoyonggang@gmail.com>
2026-01-06 15:13:16 +0800

    running bootstrap

--- bootstrap-extras.sh
+++ bootstrap-extras.sh
@@ -1,28 +1,39 @@
-pushd ./ports/libtool
-makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck --install
-popd
-
 pushd ./ports/gmp
 makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
 find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
 popd
+
+tar xf ./dist/gmp-6.3.0-3-x86_64.pkg.tar.zst -C /
+tar xf ./dist/gmp-devel-6.3.0-3-x86_64.pkg.tar.zst -C /
 
 pushd ./ports/isl
 makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
 find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
 popd
 
+tar xf ./dist/isl-0.27-2-x86_64.pkg.tar.zst -C /
+tar xf ./dist/isl-devel-0.27-2-x86_64.pkg.tar.zst -C /
+
 pushd ./ports/mpc
 makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
 find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
 popd
+
+tar xf ./dist/mpc-devel-1.3.1-2-x86_64.pkg.tar.zst -C /
+tar xf ./dist/mpc-1.3.1-2-x86_64.pkg.tar.zst -C /
 
 pushd ./ports/mpfr
 makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
 find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
 popd
 
+tar xf ./dist/mpfr-devel-4.2.2-2-x86_64.pkg.tar.zst -C /
+tar xf ./dist/mpfr-4.2.2-2-x86_64.pkg.tar.zst -C /
+
 pushd ./ports/zlib
 makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
 find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
 popd
+
+tar xf ./dist/zlib-devel-1.3.1-2-x86_64.pkg.tar.zst -C /
+tar xf ./dist/zlib-1.3.1-2-x86_64.pkg.tar.zst -C /
--- bootstrap.sh
+++ bootstrap.sh
@@ -24,10 +24,11 @@ pacman -U --overwrite \* ./dist/cmake-4.2.1-2-x86_64.pkg.tar.zst --noconfirm
 
 # Building/install binutils
 pushd ./ports/binutils
-makepkg --cleanbuild --syncdeps --force --noconfirm
-mv -f binutils-2.45.1-2-x86_64.pkg.tar.zst ../../dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst
+# makepkg --cleanbuild --syncdeps --force --noconfirm
+# mv -f binutils-2.45.1-2-x86_64.pkg.tar.zst ../../dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst
 popd
 pacman -U --overwrite \* ./dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst --noconfirm
 
 sh bootstrap-msys2-runtime.sh
+sh bootstrap-msys2-extras.sh
 sh bootstrap-gcc.sh
