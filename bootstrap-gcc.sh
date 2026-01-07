be8da8db1080888f04544d31e019c161212a8587
Yonggang Luo <luoyonggang@gmail.com>
2026-01-08 03:15:20 +0800

    gcc msys2 runtime-3.6.5-5

--- bootstrap-gcc.sh
+++ bootstrap-gcc.sh
@@ -2,7 +2,6 @@ rm -rf ./dist/gcc-libs-15.2.0-2-x86_64-stage0.pkg.tar.zst
 rm -rf ./dist/gcc-15.2.0-2-x86_64-stage0.pkg.tar.zst
 
 pushd dist-init
-# find -name "*.pkg.tar.zst" | xargs -I ARG pacman -U --overwrite \* ARG  --noconfirm
 popd
 
 pushd ./ports/gcc
--- bootstrap.sh
+++ bootstrap.sh
@@ -11,14 +11,15 @@ pacman -U --overwrite \* ./dist/cmake-4.2.1-2-x86_64.pkg.tar.zst --noconfirm
 pacman -U --overwrite \* ./dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst --noconfirm
 
 sh bootstrap-msys2-runtime.sh
-tar xf ./dist/msys2-runtime-3.6.5-4-x86_64.pkg.tar.zst -C /
-tar xf ./dist/msys2-runtime-devel-3.6.5-4-x86_64.pkg.tar.zst -C /
+tar xf ./dist/msys2-runtime-3.6.5-5-x86_64.pkg.tar.zst -C /
+tar xf ./dist/msys2-runtime-devel-3.6.5-5-x86_64.pkg.tar.zst -C /
 
 # sh bootstrap-extras.sh
 
 sh bootstrap-extras-extract.sh
 
 sh bootstrap-bash.sh
-mv bash-5.3.009-1-x86_64.pkg.tar.zst ../../dist/
+
+tar xf ./dist/bash-5.3.009-2-x86_64.pkg.tar.zst -C /
 
 # sh bootstrap-gcc.sh
