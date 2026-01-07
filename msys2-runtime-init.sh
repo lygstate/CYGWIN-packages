f117e470dca1f8235364fd8f3d6a36384dfb5bf2
Yonggang Luo <luoyonggang@gmail.com>
2026-01-07 20:00:02 +0800

    dist-init

--- msys2-runtime-init.sh
+++ msys2-runtime-init.sh
@@ -1,8 +1,8 @@
 pushd ./ports/msys2-runtime
 # updpkgsums
 makepkg --cleanbuild --syncdeps --force --noconfirm
-find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
+find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist-init/
 popd
 
-tar xf ./dist/msys2-runtime-devel-3.6.5-3-x86_64.pkg.tar.zst  -C /
-tar xf ./dist/msys2-runtime-3.6.5-3-x86_64.pkg.tar.zst  -C /
+tar xf ./dist-init/msys2-runtime-devel-3.6.5-3-x86_64.pkg.tar.zst  -C /
+tar xf ./dist-init/msys2-runtime-3.6.5-3-x86_64.pkg.tar.zst  -C /
