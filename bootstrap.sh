d05fc59fba3f8c298244312cf4136acb422e19d1
Yonggang Luo <luoyonggang@gmail.com>
2026-01-07 20:11:39 +0800

    libtool for safety, depends the old msys-runtime

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,15 +13,6 @@ makepkg --cleanbuild --syncdeps --force --noconfirm
 find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
 popd
 
-# Building/install binutils
-pushd ./ports/binutils
-makepkg --cleanbuild --syncdeps --force --noconfirm
-mv -f binutils-2.45.1-2-x86_64.pkg.tar.zst ../../dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst
-popd
-pacman -U --overwrite \* ./dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst --noconfirm
-
-sh bootstrap-msys2-runtime.sh
-
 # Building libtool
 pushd ./ports/libtool
 makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
@@ -32,6 +23,15 @@ popd
 pacman -U --overwrite \* ./dist/libltdl-2.5.4-5-x86_64.pkg.tar.zst --noconfirm
 pacman -U --overwrite \* ./dist/libtool-2.5.4-5-x86_64.pkg.tar.zst --noconfirm
 pacman -U --overwrite \* ./dist/cmake-4.2.1-2-x86_64.pkg.tar.zst --noconfirm
+
+# Building/install binutils
+pushd ./ports/binutils
+makepkg --cleanbuild --syncdeps --force --noconfirm
+mv -f binutils-2.45.1-2-x86_64.pkg.tar.zst ../../dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst
+popd
+pacman -U --overwrite \* ./dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst --noconfirm
+
+sh bootstrap-msys2-runtime.sh
 
 sh bootstrap-extras.sh
 sh bootstrap-gcc.sh
