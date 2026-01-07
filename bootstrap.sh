44617776147d684dc929783d0e77a5c12f26a108
Yonggang Luo <luoyonggang@gmail.com>
2026-01-07 23:17:06 +0800

    bootstrap-msys2-runtime.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,10 @@
 mkdir -p dist
 pushd dist-init
-find -name "*.pkg.tar.zst" | xargs -I ARG pacman -U --overwrite \* ARG  --noconfirm
+# find -name "*.pkg.tar.zst" | xargs -I ARG pacman -U --overwrite \* ARG  --noconfirm
 popd
 
 rm -rf /usr/bin/cygwin1.dll
 rm -rf /usr/lib/*.origin
-
-sh bootstrap-msys2-runtime.sh
 
 # sh bootstrap-msys2-prepare.sh
 
@@ -17,6 +15,8 @@ pacman -U --overwrite \* ./dist/cmake-4.2.1-2-x86_64.pkg.tar.zst --noconfirm
 
 # Install binutils
 pacman -U --overwrite \* ./dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst --noconfirm
+
+sh bootstrap-msys2-runtime.sh
 
 sh bootstrap-extras.sh
 sh bootstrap-gcc.sh
