2b76375e0bc824a16bdb35106ca2d006a1a942e4
Yonggang Luo <luoyonggang@gmail.com>
2026-01-07 22:02:45 +0800

    what's wrong

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,8 @@ popd
 rm -rf /usr/bin/cygwin1.dll
 rm -rf /usr/lib/*.origin
 
+sh bootstrap-msys2-runtime.sh
+
 # sh bootstrap-msys2-prepare.sh
 
 # Install cmake and libtool
@@ -15,8 +17,6 @@ pacman -U --overwrite \* ./dist/cmake-4.2.1-2-x86_64.pkg.tar.zst --noconfirm
 
 # Install binutils
 pacman -U --overwrite \* ./dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst --noconfirm
-
-sh bootstrap-msys2-runtime.sh
 
 sh bootstrap-extras.sh
 sh bootstrap-gcc.sh
