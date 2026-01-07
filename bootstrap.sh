95f5636631c2f0756e7a9f90b15c203ed6606c0f
Yonggang Luo <luoyonggang@gmail.com>
2026-01-07 20:39:54 +0800

    cmake and libtool are fine now

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,29 +6,14 @@ popd
 rm -rf /usr/bin/cygwin1.dll
 rm -rf /usr/lib/*.origin
 
-# Building cmake
-pushd ./ports/cmake
-# updpkgsums
-makepkg --cleanbuild --syncdeps --force --noconfirm
-find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
-popd
-
-# Building libtool
-pushd ./ports/libtool
-makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
-find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
-popd
+# sh bootstrap-msys2-prepare.sh
 
 # Install cmake and libtool
 pacman -U --overwrite \* ./dist/libltdl-2.5.4-5-x86_64.pkg.tar.zst --noconfirm
 pacman -U --overwrite \* ./dist/libtool-2.5.4-5-x86_64.pkg.tar.zst --noconfirm
 pacman -U --overwrite \* ./dist/cmake-4.2.1-2-x86_64.pkg.tar.zst --noconfirm
 
-# Building/install binutils
-pushd ./ports/binutils
-makepkg --cleanbuild --syncdeps --force --noconfirm
-mv -f binutils-2.45.1-2-x86_64.pkg.tar.zst ../../dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst
-popd
+# Install binutils
 pacman -U --overwrite \* ./dist/binutils-2.45.1-2-x86_64-stage0.pkg.tar.zst --noconfirm
 
 sh bootstrap-msys2-runtime.sh
