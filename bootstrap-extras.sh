8ac9312dce24f9f9598fe7f31e2595f6a7c6853a
Yonggang Luo <luoyonggang@gmail.com>
2026-01-10 14:04:26 +0800

    mpc depends on mpfr

--- bootstrap-extras.sh
+++ bootstrap-extras.sh
@@ -23,14 +23,6 @@ popd
 tar xf ./dist/isl-0.27-2-x86_64.pkg.tar.zst -C /
 tar xf ./dist/isl-devel-0.27-2-x86_64.pkg.tar.zst -C /
 
-pushd ./ports/mpc
-makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
-find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
-popd
-
-tar xf ./dist/mpc-devel-1.3.1-2-x86_64.pkg.tar.zst -C /
-tar xf ./dist/mpc-1.3.1-2-x86_64.pkg.tar.zst -C /
-
 pushd ./ports/mpfr
 makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
 find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
@@ -38,6 +30,14 @@ popd
 
 tar xf ./dist/mpfr-devel-4.2.2-2-x86_64.pkg.tar.zst -C /
 tar xf ./dist/mpfr-4.2.2-2-x86_64.pkg.tar.zst -C /
+
+pushd ./ports/mpc
+makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
+find -name "*.pkg.tar.zst" | xargs -I ARG mv -f ARG ../../dist/
+popd
+
+tar xf ./dist/mpc-devel-1.3.1-2-x86_64.pkg.tar.zst -C /
+tar xf ./dist/mpc-1.3.1-2-x86_64.pkg.tar.zst -C /
 
 pushd ./ports/zlib
 makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
