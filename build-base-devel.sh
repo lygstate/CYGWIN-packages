34fa055382f0508e460794d32319f7886a01e080
Yonggang Luo <luoyonggang@gmail.com>
2026-01-16 07:54:38 +0800

    Add support for cleanup

--- build-base-devel.sh
+++ build-base-devel.sh
@@ -1,7 +1,43 @@
 #!/bin/bash
-sh bootstrap-init.sh
+
 export MSYS_BOOTSTRAP_STAGE=stage1
+# export MSYS_BOOTSTRAP_STAGE=cleanup
+if [[ "$MSYS_BOOTSTRAP_STAGE" == "cleanup" ]]; then
+    echo "Do cleanup, do not init"
+else
+    sh bootstrap-init.sh
+fi
+
 sh msys-base-devel-list.sh
 
-# sh build-single.sh nano
-# sh build-single.sh gnupg
+do_other() {
+  sh build-single.sh nano
+  sh build-single.sh gnupg
+  sh ./build-base-devel.sh >out.txt 2>&1
+  Clean dll and exe: '.+'
+  sh ./build-all.sh >build-all.txt 2>&1
+
+  MSYS_BUILD_WITH_CLEAN=enabled MSYS_BOOTSTRAP_STAGE=stage1 sh build-single.sh jsoncpp
+  MSYS_BUILD_PKGSUMS=enabled MSYS_BUILD_WITH_CLEAN=enabled MSYS_BOOTSTRAP_STAGE=stage1 sh build-single.sh jsoncpp
+  MSYS_BUILD_PKGSUMS=enabled MSYS_BUILD_WITH_CLEAN=enabled MSYS_BOOTSTRAP_STAGE=stage1 sh build-single.sh gnupg >gnupg.txt 2>&1
+  MSYS_BUILD_PKGSUMS=enabled MSYS_BUILD_WITH_CLEAN=enabled sh build-single.sh gnupg >gnupg.txt 2>&1
+  MSYS_BUILD_PKGSUMS=enabled MSYS_BUILD_WITH_CLEAN=enabled MSYS_BOOTSTRAP_STAGE=stage1 sh build-single.sh gcc >gcc.txt 2>&1
+  MSYS_BOOTSTRAP_STAGE=stage1 sh build-single.sh gcc >gcc.txt 2>&1
+  MSYS_BOOTSTRAP_STAGE=stage1 makepkg -R
+  repack with force
+  MSYS_BOOTSTRAP_STAGE=stage1 makepkg -R -f
+
+  pacman -Syu --noconfirm --needed
+  pacman -Syu --noconfirm --needed
+  pacman -S ca-certificates --noconfirm --needed
+  update-ca-trust
+
+  pacman -Qq >msys.txt
+  updpkgsums
+  Do not extract source
+  makepkg  -e -f
+  makepkg --nobuild --cleanbuild
+  makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
+  makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck --skippgpcheck
+}
+
--- init-env.sh
+++ init-env.sh
@@ -1,29 +1,3 @@
-# sh ./build-base-devel.sh >out.txt 2>&1
-# Clean dll and exe: '.+'
-# sh ./build-all.sh >build-all.txt 2>&1
-
-# MSYS_BUILD_WITH_CLEAN=enabled MSYS_BOOTSTRAP_STAGE=stage1 sh build-single.sh jsoncpp
-# MSYS_BUILD_PKGSUMS=enabled MSYS_BUILD_WITH_CLEAN=enabled MSYS_BOOTSTRAP_STAGE=stage1 sh build-single.sh jsoncpp
-# MSYS_BUILD_PKGSUMS=enabled MSYS_BUILD_WITH_CLEAN=enabled MSYS_BOOTSTRAP_STAGE=stage1 sh build-single.sh gnupg >gnupg.txt 2>&1
-# MSYS_BUILD_PKGSUMS=enabled MSYS_BUILD_WITH_CLEAN=enabled sh build-single.sh gnupg >gnupg.txt 2>&1
-# MSYS_BUILD_PKGSUMS=enabled MSYS_BUILD_WITH_CLEAN=enabled MSYS_BOOTSTRAP_STAGE=stage1 sh build-single.sh gcc >gcc.txt 2>&1
-# MSYS_BOOTSTRAP_STAGE=stage1 sh build-single.sh gcc >gcc.txt 2>&1
-# MSYS_BOOTSTRAP_STAGE=stage1 makepkg -R
-# repack with force
-# MSYS_BOOTSTRAP_STAGE=stage1 makepkg -R -f
-
-pacman -Syu --noconfirm --needed
-pacman -Syu --noconfirm --needed
-pacman -S ca-certificates --noconfirm --needed
-update-ca-trust
-
-# pacman -Qq >msys.txt
-# updpkgsums
-# Do not extract source
-# makepkg  -e -f
-# makepkg --nobuild --cleanbuild
-# makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck
-# makepkg --cleanbuild --syncdeps --force --noconfirm --nocheck --skippgpcheck
 
 pacman -Sl msys  >msys.txt
 
