2008fe342b8f4c223a217e2721ccb87d0a60e190
Yonggang Luo <luoyonggang@gmail.com>
2026-01-22 07:35:11 +0800

    Only three packages remain filed # Failed
    ```txt
    Error for pkgbase: colordiff with retcode:4
    Error for pkgbase: opensp with retcode:4
    
    Error for pkgbase: breezy with retcode:4
    
    ```

---
+++ build-install/mingw-w64-cross-compiler-rt-finish.sh
@@ -0,0 +1,26 @@
+packages=(
+  mingw-w64-cross-crt
+  mingw-w64-cross-gcc
+  mingw-w64-cross-headers
+  mingw-w64-cross-mingw32-crt
+  mingw-w64-cross-mingw32-gcc
+  mingw-w64-cross-mingw32-headers
+  mingw-w64-cross-mingw32-winpthreads 
+  mingw-w64-cross-mingw32-winstorecompat 
+  mingw-w64-cross-mingw64-crt 
+  mingw-w64-cross-mingw64-gcc
+  mingw-w64-cross-mingw64-headers
+  mingw-w64-cross-mingw64-winpthreads 
+  mingw-w64-cross-mingw64-winstorecompat 
+  mingw-w64-cross-mingwarm64-crt
+  mingw-w64-cross-mingwarm64-gcc 
+  mingw-w64-cross-mingwarm64-headers
+  mingw-w64-cross-mingwarm64-winpthreads 
+  mingw-w64-cross-mingwarm64-winstorecompat 
+  mingw-w64-cross-winpthreads 
+  mingw-w64-cross-winpthreads  
+  mingw-w64-cross-winstorecompat
+  mingw-w64-cross-winstorecompat
+)
+
+pacman -S --noconfirm ${packages[@]}
