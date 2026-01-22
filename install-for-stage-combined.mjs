e4412846be0ed8242aa5f0859f704b9340fdfbd1
Yonggang Luo <luoyonggang@gmail.com>
2026-01-22 12:44:51 +0800

    install-for-stage-combined.mjs

--- install-for-stage-combined.mjs
+++ install-for-stage-combined.mjs
@@ -26,11 +26,10 @@ function getYYYYMMDD(date) {
   return year + month + day;
 }
 
-async function archive_full(ci_tools_root, msys_root, cache_root_cygwin) {
-  let target_msys_tar_path = path.join(
-    ci_tools_root,
-    `msys2-base-x86_64-${getYYYYMMDD(new Date())}-full.tar`,
-  );
+async function archive_full(ci_tools_root, msys_root, ci_tools_root_cygwin) {
+  const msys2_base_filename = `msys2-base-x86_64-${getYYYYMMDD(new Date())}-full.tar`;
+  let target_msys_tar_path = path.join(ci_tools_root, msys2_base_filename);
+  let target_msys_tar_path_cygwin = `${ci_tools_root_cygwin}/${msys2_base_filename}`;
   console.log(`===Compress msys64 into ${target_msys_tar_path}`);
   try {
     await fs.rm(target_msys_tar_path, { force: true, recursive: true });
@@ -55,9 +54,18 @@ async function archive_full(ci_tools_root, msys_root, cache_root_cygwin) {
       },
     },
   );
-  await spawnProcessAsync(`tar`, ["cf", target_msys_tar_path, "msys64"], {
-    cwd: ci_tools_root,
-  });
+  await spawnProcessAsync(
+    `${msys_root}/usr/bin/tar.exe`,
+    ["cf", target_msys_tar_path_cygwin, "msys64"],
+    {
+      cwd: ci_tools_root,
+      env: {
+        MSYS: "winsymlinks:native",
+        MSYSTEM: "MSYS",
+        CHERE_INVOKING: "1",
+      },
+    },
+  );
   await spawnProcessAsync(
     `${msys_root}/usr/bin/bash.exe`,
     [
@@ -67,7 +75,7 @@ async function archive_full(ci_tools_root, msys_root, cache_root_cygwin) {
         `mkdir -p /var/cache/pacman/pkg`,
         `rm -rf /var/cache/pacman/pkg`,
         `cd /var/cache/pacman/`,
-        `ln -s -T ${cache_root_cygwin}/var-cache/pacman/pkg pkg`,
+        `ln -s -T ${ci_tools_root_cygwin}/var-cache/pacman/pkg pkg`,
       ].join("; "),
     ],
     {
@@ -90,56 +98,11 @@ async function clear_msys64(msys_root) {
   }
 }
 
-async function main() {
-  const ci_tools_root = "C:/CI-Tools";
-  const msys_root = path.join(ci_tools_root, "msys64");
-  const pkg_root = __dirname;
-
-  await clear_msys64(msys_root);
-
-  console.log(`===Init env`);
-  let has_msys64 = false;
-  try {
-    await fs.access(msys_root);
-    has_msys64 = true;
-  } catch (e) {}
-  if (!has_msys64) {
-    console.log(`===Extracting base`);
-    await spawnProcessAsync(
-      `tar`,
-      ["xf", path.join(ci_tools_root, "msys2-base-x86_64-20251213.tar")],
-      {
-        cwd: ci_tools_root,
-      },
-    );
-    console.log("Extract base finished\n");
-  }
-
-  await spawnProcessAsync(
-    `${msys_root}/usr/bin/bash.exe`,
-    [
-      "--login",
-      "-c",
-      `echo "Init msys with $MSYSTEM finished"; rm -rf /var/lib/pacman/db.lck; pacman -Syu --noconfirm`,
-    ],
-    {
-      env: {
-        MSYSTEM: "MSYS",
-      },
-    },
-  );
-  const pkg_root_cygwin = (
-    await spawnProcessAsyncCapture(`${msys_root}/usr/bin/cygpath.exe`, [
-      pkg_root,
-    ])
-  ).stdout.trim();
-
-  const cache_root_cygwin = (
-    await spawnProcessAsyncCapture(`${msys_root}/usr/bin/cygpath.exe`, [
-      ci_tools_root,
-    ])
-  ).stdout.trim();
-
+async function install_packages(
+  msys_root,
+  ci_tools_root_cygwin,
+  ci_tools_root,
+) {
   console.log(`===Init env finished then upgrade base packages`);
 
   await spawnProcessAsync(
@@ -151,9 +114,9 @@ async function main() {
         `mkdir -p /var/cache/pacman/pkg`,
         `rm -rf /var/cache/pacman/pkg`,
         `cd /var/cache/pacman/`,
-        `ln -s -T ${cache_root_cygwin}/var-cache/pacman/pkg pkg`,
+        `ln -s -T ${ci_tools_root_cygwin}/var-cache/pacman/pkg pkg`,
         `cat /etc/pacman.conf | grep ^SigLevel`,
-        `cd ${cache_root_cygwin}/`,
+        `cd ${ci_tools_root_cygwin}/`,
         `pacman -Syu --noconfirm`,
       ].join("; "),
     ],
@@ -174,21 +137,88 @@ async function main() {
       [
         "--login",
         "-c",
-        "ls; pacman -U --noconfirm --overwrite \\* `ls | tr '\n' ' '`",
+        "export MSYS=winsymlinks:native; pacman -U --noconfirm --overwrite \\* `ls | tr '\n' ' '`",
       ],
       {
         cwd: path.join(ci_tools_root, "cygwin-dist"),
         env: {
-          MSYS: "winsymlinks:native",
-          MSYSTEM: "MSYS",
+          MSYSTEM: "CYGWIN",
           CHERE_INVOKING: "1",
         },
       },
     );
   }
+}
+
+async function prepare_base(has_msys64, ci_tools_root, msys_root) {
+  if (!has_msys64) {
+    console.log(`===Extracting base`);
+    await spawnProcessAsync(
+      `tar`,
+      ["xf", path.join(ci_tools_root, "msys2-base-x86_64-20251213.tar")],
+      {
+        cwd: ci_tools_root,
+      },
+    );
+    console.log("Extract base finished\n");
+  }
+  await spawnProcessAsync(
+    `${msys_root}/usr/bin/bash.exe`,
+    [
+      "--login",
+      "-c",
+      `echo "Init msys with $MSYSTEM finished"; rm -rf /var/lib/pacman/db.lck; pacman -Syu --noconfirm`,
+    ],
+    {
+      env: {
+        MSYSTEM: "MSYS",
+      },
+    },
+  );
+}
+
+async function main() {
+  const ci_tools_root = "C:/CI-Tools";
+  const msys_root = path.join(ci_tools_root, "msys64");
+
+  await clear_msys64(msys_root);
+
+  console.log(`===Init env`);
+  let has_msys64 = false;
+  try {
+    await fs.access(msys_root);
+    has_msys64 = true;
+  } catch (e) {}
+
+  await prepare_base(has_msys64, ci_tools_root, msys_root);
+  const ci_tools_root_cygwin = (
+    await spawnProcessAsyncCapture(`${msys_root}/usr/bin/cygpath.exe`, [
+      ci_tools_root,
+    ])
+  ).stdout.trim();
 
   if (!has_msys64) {
-    await archive_full(ci_tools_root, msys_root, cache_root_cygwin);
+    await install_packages(msys_root, ci_tools_root_cygwin, ci_tools_root);
+  }
+
+  await spawnProcessAsync(
+    `${msys_root}/usr/bin/bash.exe`,
+    [
+      "--login",
+      "-c",
+      "export MSYS=winsymlinks:native; pacman -U --noconfirm --overwrite \\* zsh-5.9-4-x86_64.pkg.tar.zst grml-zsh-config-0.19.26-2-any.pkg.tar.zst",
+    ],
+    {
+      cwd: path.join(ci_tools_root, "cygwin-dist"),
+      env: {
+        MSYSTEM: "CYGWIN",
+        CHERE_INVOKING: "1",
+      },
+    },
+  );
+
+  if (!has_msys64) {
+    await archive_full(ci_tools_root, msys_root, ci_tools_root_cygwin);
   }
   process.exit(0);
 }
