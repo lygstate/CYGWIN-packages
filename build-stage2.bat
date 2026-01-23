81b19f40174ce5fdcc5b0bfe3b6d22e33cc801d8
Yonggang Luo <luoyonggang@gmail.com>
2026-01-23 11:21:37 +0800

    rustc version

--- build-stage2.bat
+++ build-stage2.bat
@@ -10,16 +10,17 @@ touch build-cache\stage2\gcc-stage2-build-finished.build
 bash --login -c "MSYS_BUILD_NOEXTRACT=enabled MSYS_BUILD_PKGSUMS=enabled sh build-single.sh gcc >build-stage2-gcc.txt 2>&1"
 
 echo "Building rust by cross"
-if exist dist-rust/rust-1.92.0-3-x86_64.pkg.tar.zst (
+set RUST_VERSION=1.93.0-2
+if exist dist-rust/rust-%RUST_VERSION%-x86_64.pkg.tar.zst (
   echo "Building rust by cross use exist"
-  cp -arf dist-rust/rust-1.92.0-3-x86_64-cross.pkg.tar.zst ports/rust/rust-1.92.0-3-x86_64.pkg.tar.zst
+  cp -arf dist-rust/rust-%RUST_VERSION%-x86_64-cross.pkg.tar.zst ports/rust/rust-%RUST_VERSION%-x86_64.pkg.tar.zst
   touch build-cache\stage2\rust-stage2-build-finished.build
 ) else (
   echo "Building the rust by cross clearly"
   rm -rf build-cache\stage2\rust-stage2-build-finished.build
 )
 bash --login -c "MSYS_BUILD_NOEXTRACT=enabled MSYS_BUILD_PKGSUMS=enabled MSYS_BOOTSTRAP_RUST=enabled sh build-single.sh rust >build-stage2-rust0.txt 2>&1"
-cp -arf ports/rust/rust-1.92.0-3-x86_64.pkg.tar.zst dist-rust/rust-1.92.0-3-x86_64-cross.pkg.tar.zst
+cp -arf ports/rust/rust-%RUST_VERSION%-x86_64.pkg.tar.zst dist-rust/rust-%RUST_VERSION%-x86_64-cross.pkg.tar.zst
 
 :: The rebaseall command (often from Cygwin's autorebase) doesn't store results in a typical user-facing file; 
 :: instead, it modifies the in-memory image of system DLLs and updates the Windows Registry
@@ -38,16 +39,15 @@ set MSYSTEM=CYGWIN
 set CHERE_INVOKING=1
 
 echo "Building the rust by native"
-if exist dist-rust/rust-1.92.0-3-x86_64.pkg.tar.zst (
+if exist dist-rust/rust-%RUST_VERSION%-x86_64.pkg.tar.zst (
   echo "Building rust by native use exist"
-  cp -arf dist-rust/rust-1.92.0-3-x86_64.pkg.tar.zst ports/rust/rust-1.92.0-3-x86_64.pkg.tar.zst
+  cp -arf dist-rust/rust-%RUST_VERSION%-x86_64.pkg.tar.zst ports/rust/rust-%RUST_VERSION%-x86_64.pkg.tar.zst
 ) else (
-  rd /s /q ports\rust\src\rustc-1.92.0-src\cygwin-build-11
   echo "Building the rust by native clearly"
   rm -rf build-cache\stage2\rust-stage2-build-finished.build
 )
 bash --login -c "MSYS_BUILD_NOEXTRACT=enabled MSYS_BUILD_PKGSUMS=enabled sh build-single.sh rust >build-stage2-rust2.txt 2>&1"
-cp -arf ports/rust/rust-1.92.0-3-x86_64.pkg.tar.zst dist-rust/rust-1.92.0-3-x86_64.pkg.tar.zst
+cp -arf ports/rust/rust-%RUST_VERSION%-x86_64.pkg.tar.zst dist-rust/rust-%RUST_VERSION%-x86_64.pkg.tar.zst
 
 :: goto :eof
 :: bash --login -c "rm -rf /etc/rebase.db.x86_64"
@@ -58,7 +58,7 @@ set MSYS=winsymlinks:native
 set MSYSTEM=CYGWIN
 set CHERE_INVOKING=1
 pushd dist-final
-bash --login -c "pacman -U --noconfirm --overwrite \* ./rust-1.92.0-3-x86_64.pkg.tar.zst ./gcc-libs-15.2.0-2-x86_64.pkg.tar.zst ./gcc-15.2.0-2-x86_64.pkg.tar.zst"
+bash --login -c "pacman -U --noconfirm --overwrite \* ./rust-%RUST_VERSION%-x86_64.pkg.tar.zst ./gcc-libs-15.2.0-2-x86_64.pkg.tar.zst ./gcc-15.2.0-2-x86_64.pkg.tar.zst"
 popd
 
 ::bash --login -c "MSYS_BUILD_WITH_CLEAN=enabled MSYS_BUILD_PKGSUMS=enabled sh build-single.sh cargo-c"
